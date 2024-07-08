31b58591b517dad347e20977f7ed445c170d640a
Vishal Goswami <vishalgoswami2250@gmail.com>
2024-07-08 14:03:08 +0530

    starportal

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
+
+import './style.css'
+import Home from './views/Home'
+import NotFound from './views/not-Found'
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route component={Home} exact path="/" />
+        <Route component={NotFound} path="**" />
+        <Redirect to="**" />
+      </Switch>
+    </Router>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
